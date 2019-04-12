b45df65c819bd5df6605db7f1e6f7f7dbbf533cb
Jason Quense <monastic.panic@gmail.com>
2019-04-12 16:11:47 -0400

    fix casing

--- test/unit/components/date.js
+++ test/unit/components/date.js
@@ -1,165 +1,165 @@
-import React from 'react'
-import { mount } from 'enzyme'
-import { mountWithContext, generateIntlContext } from '../testUtils'
-import FormattedDate from '../../../src/components/date'
+import React from 'react';
+import {mount} from 'enzyme';
+import {mountWithContext, generateIntlContext} from '../testUtils';
+import FormattedDate from '../../../src/components/date';
 
 describe('<FormattedDate>', () => {
-  let consoleError
-  let intl
+  let consoleError;
+  let intl;
 
   beforeEach(() => {
-    consoleError = jest.spyOn(console, 'error').mockImplementation(() => {})
+    consoleError = jest.spyOn(console, 'error').mockImplementation(() => {});
     intl = generateIntlContext({
       locale: 'en',
-    })
-  })
+    });
+  });
 
   afterEach(() => {
-    consoleError.mockRestore()
-  })
+    consoleError.mockRestore();
+  });
 
   it('has a `displayName`', () => {
-    expect(typeof FormattedDate.displayName).toBe('string')
-  })
+    expect(typeof FormattedDate.displayName).toBe('string');
+  });
 
   it('throws when <IntlProvider> is missing from ancestry', () => {
     expect(() => mount(<FormattedDate />)).toThrow(
       '[React Intl] Could not find required `intl` object. <IntlProvider> needs to exist in the component ancestry.'
-    )
-  })
+    );
+  });
 
   it('requires a finite `value` prop', () => {
-    const value = Date.now()
+    const value = Date.now();
 
-    mountWithContext(intl, <FormattedDate value={value} />)
+    mountWithContext(intl, <FormattedDate value={value} />);
 
-    expect(isFinite(value)).toBe(true)
+    expect(isFinite(value)).toBe(true);
 
-    expect(consoleError).not.toHaveBeenCalled()
+    expect(consoleError).not.toHaveBeenCalled();
 
-    mountWithContext(intl, <FormattedDate />)
+    mountWithContext(intl, <FormattedDate />);
 
-    expect(consoleError).toHaveBeenCalledTimes(1)
-    expect(consoleError).toHaveBeenCalledWith(
-      '[React Intl] Error formatting date.\nRangeError: Invalid time value'
-    )
-  })
+    expect(consoleError).toHaveBeenCalledTimes(1);
+    expect(consoleError.mock.calls[0][0]).toContain(
+      '[React Intl] Error formatting date.\nRangeError:'
+    );
+  });
 
   it('renders a formatted date in a <span>', () => {
-    const date = Date.now()
+    const date = Date.now();
 
-    const rendered = mountWithContext(intl, <FormattedDate value={date} />)
+    const rendered = mountWithContext(intl, <FormattedDate value={date} />);
 
-    expect(rendered.find('span').length).toEqual(1)
-    expect(rendered.text()).toBe(intl.formatDate(date))
-  })
+    expect(rendered.find('span').length).toEqual(1);
+    expect(rendered.text()).toBe(intl.formatDate(date));
+  });
 
   it('should not re-render when props are the same', () => {
-    const date = Date.now()
+    const date = Date.now();
 
-    const spy = jest.fn(() => null)
+    const spy = jest.fn(() => null);
     const withInlContext = mountWithContext(
       intl,
       <FormattedDate value={date}>{spy}</FormattedDate>
-    )
+    );
 
     withInlContext.setProps({
       ...withInlContext.props(),
-    })
+    });
 
-    expect(spy).toHaveBeenCalledTimes(1)
-  })
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
 
   it('should re-render when props change', () => {
-    const date = Date.now()
+    const date = Date.now();
 
-    const spy = jest.fn(() => null)
+    const spy = jest.fn(() => null);
     const withInlContext = mountWithContext(
       intl,
       <FormattedDate value={date}>{spy}</FormattedDate>
-    )
+    );
 
     withInlContext.setProps({
       ...withInlContext.props(),
       value: withInlContext.prop('value') + 1,
-    })
+    });
 
-    expect(spy).toHaveBeenCalledTimes(2)
-  })
+    expect(spy).toHaveBeenCalledTimes(2);
+  });
 
   xit('should re-render when context changes', () => {
-    const date = Date.now()
+    const date = Date.now();
 
-    const spy = jest.fn(() => null)
+    const spy = jest.fn(() => null);
     const withInlContext = mount(
       <FormattedDate value={date}>{spy}</FormattedDate>
-    )
+    );
 
     const otherIntl = generateIntlContext({
       locale: 'en-US',
-    })
-    withInlContext.instance().mockContext(otherIntl)
+    });
+    withInlContext.instance().mockContext(otherIntl);
 
-    expect(spy).toHaveBeenCalledTimes(2)
-  })
+    expect(spy).toHaveBeenCalledTimes(2);
+  });
 
   it('accepts valid Intl.DateTimeFormat options as props', () => {
-    const date = new Date()
-    const options = { year: 'numeric' }
+    const date = new Date();
+    const options = {year: 'numeric'};
 
     const rendered = mountWithContext(
       intl,
       <FormattedDate value={date} {...options} />
-    )
+    );
 
-    expect(rendered.text()).toBe(intl.formatDate(date, options))
-  })
+    expect(rendered.text()).toBe(intl.formatDate(date, options));
+  });
 
   it('fallsback and warns on invalid Intl.DateTimeFormat options', () => {
-    const date = new Date()
+    const date = new Date();
     const rendered = mountWithContext(
       intl,
       <FormattedDate value={date} year="invalid" />
-    )
+    );
 
-    expect(rendered.text()).toBe(String(date))
-    expect(consoleError).toHaveBeenCalled()
-  })
+    expect(rendered.text()).toBe(String(date));
+    expect(consoleError).toHaveBeenCalled();
+  });
 
   it('accepts `format` prop', () => {
     intl = generateIntlContext({
       locale: 'en',
       formats: {
         date: {
-          'year-only': { year: 'numeric' },
+          'year-only': {year: 'numeric'},
         },
       },
-    })
+    });
 
-    const date = new Date()
-    const format = 'year-only'
+    const date = new Date();
+    const format = 'year-only';
 
     const rendered = mountWithContext(
       intl,
       <FormattedDate value={date} format={format} />
-    )
+    );
 
-    expect(rendered.text()).toBe(intl.formatDate(date, { format }))
-  })
+    expect(rendered.text()).toBe(intl.formatDate(date, {format}));
+  });
 
   it('supports function-as-child pattern', () => {
-    const date = Date.now()
+    const date = Date.now();
 
-    const spy = jest.fn(() => <b>Jest</b>)
+    const spy = jest.fn(() => <b>Jest</b>);
     const rendered = mountWithContext(
       intl,
       <FormattedDate value={date}>{spy}</FormattedDate>
-    )
+    );
 
-    expect(spy).toHaveBeenCalledTimes(1)
-    expect(spy).toHaveBeenCalledWith(intl.formatDate(date))
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(intl.formatDate(date));
 
-    expect(rendered.find('b').length).toEqual(1)
-    expect(rendered.text()).toBe('Jest')
-  })
-})
+    expect(rendered.find('b').length).toEqual(1);
+    expect(rendered.text()).toBe('Jest');
+  });
+});
--- test/unit/testUtils.js
+++ test/unit/testUtils.js
@@ -1,44 +1,44 @@
-import React from 'react'
-import { mount } from 'enzyme'
-import IntlProvider from '../../src/components/Provider'
-import useIntl, { Provider } from '../../src/components/useIntl'
+import React from 'react';
+import {mount} from 'enzyme';
+import IntlProvider from '../../src/components/provider';
+import useIntl, {Provider} from '../../src/components/useIntl';
 
-function Wrapper({ intl, element, ...props }) {
-  return <Provider value={intl}>{React.cloneElement(element, props)}</Provider>
+function Wrapper({intl, element, ...props}) {
+  return <Provider value={intl}>{React.cloneElement(element, props)}</Provider>;
 }
 
 export function mountWithContext(intl, element, options) {
-  return mount(<Wrapper intl={intl} element={element} />, options)
+  return mount(<Wrapper intl={intl} element={element} />, options);
 }
 
 export class SpyComponent extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
-    this._renders = 0
+    this._renders = 0;
   }
 
   getRenderCount() {
-    return this._renders
+    return this._renders;
   }
 
   render() {
-    this._renders++
+    this._renders++;
 
-    return null
+    return null;
   }
 }
 
 export const generateIntlContext = intl => {
-  let context
+  let context;
   const Child = () => {
-    context = useIntl()
-    return null
-  }
+    context = useIntl();
+    return null;
+  };
   mount(
     <IntlProvider {...intl}>
       <Child />
     </IntlProvider>
-  ).unmount()
-  return context
-}
+  ).unmount();
+  return context;
+};
